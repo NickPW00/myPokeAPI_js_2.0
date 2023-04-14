4c183b11b84bdedc05d7dc74cf0dc0f40bfc2583
Nicholas Puggian <nicholas.wright@al.infnet.edu.br>
2023-04-14 19:13:28 -0300

    Reorganização do css e adição das estatísticas.

--- script.js
+++ script.js
@@ -14,17 +14,26 @@ const fetchPokemon = () => {
             console.log(pokemons[0], `https://pokeapi.co/api/v2/pokemon-species/0/`)
             let cardsPokemon = pokemons.reduce((accumulator, {name, id, types, abilities, stats}) => {
                 let tipos = types.map(typeInfo => typeInfo.type.name).join(" | ");
+                let tiposClass = types.map(typeInfo => typeInfo.type.name);
                 let imagem = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`; ''
                 let habilidades = abilities.map(abilInfo => abilInfo.ability.name).join(" | ");
                 let estatisticas = stats.map(a => a.base_stat).join(" | ");
+                let estatisticasSep = stats.map(a => a.base_stat);
                 accumulator += 
                 `
-                <div class="card">
+                <div class="card ${tiposClass[0]}">
                     <img src=${imagem}>
                     <p>${id}. ${name}</p>
                     <p>${tipos}</p>
                     <p>${habilidades}</p>
-                    <p>${estatisticas}</p>
+                    <ul>
+                        <li>HP.:${estatisticasSep[0]}</li>
+                        <li>ATK:${estatisticasSep[1]}</li>
+                        <li>DEF:${estatisticasSep[2]}</li>
+                        <li>SP. ATK.:${estatisticasSep[3]}</li>
+                        <li>SP. DEF${estatisticasSep[4]}</li>
+                        <li>SPEED:${estatisticasSep[5]}</li>
+                    </ul>
                     <p></p>
                 </div>
                 `
