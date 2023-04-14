a40d2eda21400293011da644a25e881bb92e4539
Nicholas Puggian <nicholas.wright@al.infnet.edu.br>
2023-04-14 15:33:34 -0300

    Adições ao css der estilo, e posto status e abilities no js.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const pokemonPromises = [];
+let body = document.querySelector("body");
+let conjuntoCards = document.querySelector("#conjuntoCards");
 
 const fetchPokemon = () => {
     const getPokemonUrl = id =>`https://pokeapi.co/api/v2/pokemon/${id}`
@@ -10,17 +12,32 @@ const fetchPokemon = () => {
     Promise.all(pokemonPromises)
         .then(pokemons => {
             console.log(pokemons[0])
-            pokemons.forEach(
-                pokemon => {
-                    console.log(pokemon.name , "/",
-                                pokemon.id, "/",
-                                pokemon.types.map(typeInfo => typeInfo.type.name).join(" | "), "/",
-                                pokemon.abilities.map(abilInfo => abilInfo.ability.name).join(" | "), "/",
-                                pokemon.stats.map(a => a.base_stat).join(" | ")) 
-                    /* pokemon.types.forEach(a => console.log(pokemon.name, a.type.name)) */
-                })
+            let cardsPokemon = pokemons.reduce((accumulator, {name, id, types, abilities, stats}) => {
+                let tipos = types.map(typeInfo => typeInfo.type.name).join(" | ");
+                let imagem = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`; ''
+                let habilidades = abilities.map(abilInfo => abilInfo.ability.name).join(" | ");
+                let estatisticas = stats.map(a => a.base_stat).join(" | ");
+                accumulator += 
+                `
+                <div class="card">
+                    <img src=${imagem}>
+                    <p>${id}. ${name}</p>
+                    <p>${tipos}</p>
+                    <p>${habilidades}</p>
+                    <p>${estatisticas}</p>
+                </div>
+                `
+                return accumulator
+            }, '')
+            conjuntoCards.innerHTML = cardsPokemon
         })
 }
 
 fetchPokemon()
-
+/* 
+pokemon.name,
+pokemon.id,
+pokemon.types.map(typeInfo => typeInfo.type.name).join(" | "),
+pokemon.abilities.map(abilInfo => abilInfo.ability.name).join(" | "),
+pokemon.stats.map(a => a.base_stat).join(" | "))  */
+/* pokemon.types.forEach(a => console.log(pokemon.name, a.type.name)) */
