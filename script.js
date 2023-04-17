98bbeede7f75ec19ea76d7c7ec1f8c0184baab6c
Nicholas Puggian <nicholas.wright@al.infnet.edu.br>
2023-04-17 12:27:26 -0300

    Remodelamento do HTML, adição de peso e altura e começo de uma nova 'fetch'.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const pokemonPromises = [];
+
 let body = document.querySelector("body");
 let conjuntoCards = document.querySelector("#conjuntoCards");
 
 const fetchPokemon = () => {
     const getPokemonUrl = id =>`https://pokeapi.co/api/v2/pokemon/${id}`
-    
     for(let i = 1; i <= 251; i++){
         pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()))
     };
@@ -12,35 +12,43 @@ const fetchPokemon = () => {
     Promise.all(pokemonPromises)
         .then(pokemons => {
             console.log(pokemons[0], `https://pokeapi.co/api/v2/pokemon-species/0/`)
-            let cardsPokemon = pokemons.reduce((accumulator, {name, id, types, abilities, stats}) => {
+            let cardsPokemon = pokemons.reduce((accumulator, {name, id, types, abilities, stats, height, weight}) => {
                 let tipos = types.map(typeInfo => typeInfo.type.name).join(" | ");
                 let tiposClass = types.map(typeInfo => typeInfo.type.name);
                 let imagem = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
-                let imagemTipo = `https://pokeapi.co/api/v2/type/12/`;
                 let habilidades = abilities.map(abilInfo => abilInfo.ability.name).join(" | ");
-                let estatisticas = stats.map(a => a.base_stat).join(" | ");
                 let estatisticasSep = stats.map(a => a.base_stat);
                 accumulator += 
                 `
-                <div class="card ${tiposClass[0]}">
-                    <div class="fundo">
-                        <img src=${imagem}>
+                    <div class="card ${tiposClass[0]}">
+                        <div class="fundo">
+                            <img src=${imagem}>
+                        </div>
+                        <div>
+                            <p>${id}. ${name[0].toUpperCase() + name.substring(1)}</p>
+                            <p>${tipos}</p>
+                            <p> 
+                                height: ${height/10} m<br>
+                                weight: ${weight/10} kg
+                            </p>
+                        </div>
+                        <div>
+                            <p>Habilidades:</p>
+                            <p>${habilidades}</p>
+                        </div>
+                        <div>
+                        <p>Estatísticas:</p>
+                            <ul>
+                                <li>HP.:${estatisticasSep[0]}</li>
+                                <li>ATK:${estatisticasSep[1]}</li>
+                                <li>DEF:${estatisticasSep[2]}</li>
+                                <li>SP. ATK:${estatisticasSep[3]}</li>
+                                <li>SP. DEF:${estatisticasSep[4]}</li>
+                                <li>SPEED:${estatisticasSep[5]}</li>
+                            </ul>
+                        </div>
+                        
                     </div>
-                    <div>
-                        <p>${id}. ${name}</p>
-                        <p>${tipos}</p>
-                    </div>
-                    <p>${habilidades}</p>
-                    <ul>
-                        <li>HP.:${estatisticasSep[0]}</li>
-                        <li>ATK:${estatisticasSep[1]}</li>
-                        <li>DEF:${estatisticasSep[2]}</li>
-                        <li>SP. ATK.:${estatisticasSep[3]}</li>
-                        <li>SP. DEF${estatisticasSep[4]}</li>
-                        <li>SPEED:${estatisticasSep[5]}</li>
-                    </ul>
-                    <p></p>
-                </div>
                 `
                 return accumulator
             }, '')
