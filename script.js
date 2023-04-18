ea0d01892e370bd48ffdfb358400521cc2f3d77c
Nicholas Puggian <nicholas.wright@al.infnet.edu.br>
2023-04-18 16:35:16 -0300

    Versão pronta para merge: Novo sistema de integração de arrays e objetos feita, se tornado uma só e, assim, facilitando na sua busca.

--- script.js
+++ script.js
@@ -14,9 +14,57 @@ const fetchPokemon = () => {
     };
     
     Promise.all([...pokemonPromises, ...pokemonSpeciePromises])
-        .then((result) => {
-            for(let i = 0; i < qtdPokemons; i++)
-            console.log(result[i].name, result[qtdPokemons + i].flavor_text_entries[0].flavor_text)
+        .then(pokemons => {
+            let pokemonArray = [];
+            for(let i = 0; i < qtdPokemons; i++){
+                pokemonArray.push(Object.assign({} ,pokemons[i], pokemons[qtdPokemons + i]));
+            }
+            console.log(pokemonArray[0])
+            let cardsPokemon = pokemonArray.reduce((accumulator, {name, id, types, abilities, stats, height, weight, flavor_text_entries}) => {
+                let tipos = types.map(typeInfo => typeInfo.type.name).join(" | ");
+                let tiposClass = types.map(typeInfo => typeInfo.type.name);
+                let imagem = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+                let habilidades = abilities.map(abilInfo => abilInfo.ability.name).join(" | ");
+                let estatisticasSep = stats.map(a => a.base_stat);
+                accumulator += 
+                `
+                    <div>
+                        <div class="card ${tiposClass[0]}">
+                            <div class="fundo">
+                                <img src=${imagem}>
+                            </div>
+                            <div>
+                                <p>${id}. ${name[0].toUpperCase() + name.substring(1)}</p>
+                                <p>${tipos}</p>
+                                <p> 
+                                    height: ${height/10} m<br>
+                                    weight: ${weight/10} kg
+                                </p>
+                            </div>
+                            <div>
+                                <p>Habilidades:</p>
+                                <p>${habilidades}</p>
+                            </div>
+                            <div>
+                            <p>Estatísticas:</p>
+                                <ul>
+                                    <li>HP.:${estatisticasSep[0]}</li>
+                                    <li>ATK:${estatisticasSep[1]}</li>
+                                    <li>DEF:${estatisticasSep[2]}</li>
+                                    <li>SP. ATK:${estatisticasSep[3]}</li>
+                                    <li>SP. DEF:${estatisticasSep[4]}</li>
+                                    <li>SPEED:${estatisticasSep[5]}</li>
+                                </ul>
+                            </div>
+                            <div>
+                                <p>${flavor_text_entries[9].flavor_text}</p>
+                            </div>
+                        </div>
+                    </div>
+                `
+                return accumulator
+            }, '')
+            conjuntoCards.innerHTML = cardsPokemon
         }
     )
 }
