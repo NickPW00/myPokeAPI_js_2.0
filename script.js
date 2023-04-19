2e8394669305c365159d7fb5d8d45d1ca43da6f5
Nicholas Puggian <nicholas.wright@al.infnet.edu.br>
2023-04-19 15:21:04 -0300

    Começo da organização do codigo e modelamento para caber o texto descritivo.

--- script.js
+++ script.js
@@ -1,35 +1,60 @@
-const qtdPokemons = 251
+const qtdPokemons = 251;
 const pokemonPromises = [];
 const pokemonSpeciePromises = [];
 
-let body = document.querySelector("body");
-let conjuntoCards = document.querySelector("#conjuntoCards");
+const body = document.querySelector("body");
+const conjuntoCards = document.querySelector("#conjuntoCards");
+
+const requisitorDePromises = _ => {
+    const getPokemonUrl = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`;
+	const getPokemonSpecieUrl = (id) =>
+		`https://pokeapi.co/api/v2/pokemon-species/${id}`;
+	for (let i = 1; i <= qtdPokemons; i++) {
+		pokemonPromises.push(
+			fetch(getPokemonUrl(i)).then((response) => response.json())
+		);
+		pokemonSpeciePromises.push(
+			fetch(getPokemonSpecieUrl(i)).then((response) => response.json())
+		);
+	}
+}
 
 const fetchPokemon = () => {
-    const getPokemonUrl = id =>`https://pokeapi.co/api/v2/pokemon/${id}`
-    const getPokemonSpecieUrl = id =>`https://pokeapi.co/api/v2/pokemon-species/${id}`
-    for(let i = 1; i <= qtdPokemons; i++){
-        pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()))
-        pokemonSpeciePromises.push(fetch(getPokemonSpecieUrl(i)).then(response => response.json()))
-    };
-    
-    Promise.all([...pokemonPromises, ...pokemonSpeciePromises])
-        .then(pokemons => {
-            let pokemonArray = [];
-            for(let i = 0; i < qtdPokemons; i++){
-                pokemonArray.push(Object.assign({} ,pokemons[i], pokemons[qtdPokemons + i]));
-            }
-            console.log(pokemonArray[0])
-            let cardsPokemon = pokemonArray.reduce((accumulator, {name, id, types, abilities, stats, height, weight, flavor_text_entries}) => {
-                let tipos = types.map(typeInfo => typeInfo.type.name).join(" | ");
-                let tiposClass = types.map(typeInfo => typeInfo.type.name);
-                let imagem = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
-                let habilidades = abilities.map(abilInfo => abilInfo.ability.name).join(" | ");
-                let estatisticasSep = stats.map(a => a.base_stat);
-                accumulator += 
-                `
-                    <div>
-                        <div class="card ${tiposClass[0]}">
+	requisitorDePromises()
+
+	Promise.all([...pokemonPromises, ...pokemonSpeciePromises])
+		.then((pokemons) => {
+			let pokemonArray = [];
+			for (let i = 0; i < qtdPokemons; i++) {
+				pokemonArray.push(
+					Object.assign({}, pokemons[i], pokemons[qtdPokemons + i])
+				);
+			}
+			console.log(pokemonArray[0]);
+			let cardsPokemon = pokemonArray.reduce(
+				(
+					accumulator,
+					{
+						name,
+						id,
+						types,
+						abilities,
+						stats,
+						height,
+						weight,
+						flavor_text_entries: descricao,
+					}
+				) => {
+					let tipos = types.map((typeInfo) => typeInfo.type.name).join(" | ");
+					let tiposClass = types.map((typeInfo) => typeInfo.type.name);
+					let imagem = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+					let habilidades = abilities
+						.map((abilInfo) => abilInfo.ability.name)
+						.join(" | ");
+					let estatisticasSep = stats.map((a) => a.base_stat);
+					accumulator += `
+                    <div class="card ${tiposClass[0]}">
+                        <div class="cardP1">
                             <div class="fundo">
                                 <img src=${imagem}>
                             </div>
@@ -37,8 +62,8 @@ const fetchPokemon = () => {
                                 <p>${id}. ${name[0].toUpperCase() + name.substring(1)}</p>
                                 <p>${tipos}</p>
                                 <p> 
-                                    height: ${height/10} m<br>
-                                    weight: ${weight/10} kg
+                                    height: ${height / 10} m<br>
+                                    weight: ${weight / 10} kg
                                 </p>
                             </div>
                             <div>
@@ -56,20 +81,22 @@ const fetchPokemon = () => {
                                     <li>SPEED:${estatisticasSep[5]}</li>
                                 </ul>
                             </div>
-                            <div>
-                                <p>${flavor_text_entries[9].flavor_text}</p>
-                            </div>
+                        </div>
+                        <div class="descricaoPokemon">
+                            <p>${descricao[9].flavor_text}</p>
                         </div>
                     </div>
-                `
-                return accumulator
-            }, '')
-            conjuntoCards.innerHTML = cardsPokemon
-        }
-    )
-}
+                `;
+					return accumulator;
+				},
+				""
+			);
+			conjuntoCards.innerHTML = cardsPokemon;
+		})
+};
 
-fetchPokemon()
+fetchPokemon();
+
 /* 
 pokemon.name,
 pokemon.id,
