384ff064475f9c660dc2acfe7dd7bc068a6a95c3
Nicholas Puggian <nicholas.wright@al.infnet.edu.br>
2023-04-20 16:53:29 -0300

    Versão pronta para dar merge.

--- script.js
+++ script.js
@@ -60,7 +60,10 @@ const gerarHTML = array => {
                     </div>
                     <div>
                         <p>${id}. ${name[0].toUpperCase() + name.substring(1)}</p>
-                        <p>${tipos}</p>
+                        <div class='typeContainer'>
+                            <img src='poketypesV2/${tiposClass[0]}.png' alt='tipo primário: ${tiposClass[0]}'>
+                            <img src='poketypesV2/${tiposClass[1]}.png' alt='tipo secundário: ${tiposClass[1]}' onerror="this.style.display='none'">
+                        </div>
                         <p> 
                             height: ${height / 10} m<br>
                             weight: ${weight / 10} kg
@@ -95,7 +98,7 @@ const gerarHTML = array => {
 }
 
 const montadorDeArray = pokemons => {
-        let pokemonArray = integradoraDeArrays(pokemons);
+        var pokemonArray = integradoraDeArrays(pokemons);
         console.log(pokemonArray[0]);
         gerarHTML(pokemonArray)
 }
@@ -104,56 +107,3 @@ const arrayDePromises = requisitorDePromises()
 
 Promise.all(arrayDePromises)
     .then(montadorDeArray)
-
-/* 
-pokemon.name,
-pokemon.id,
-pokemon.types.map(typeInfo => typeInfo.type.name).join(" | "),
-pokemon.abilities.map(abilInfo => abilInfo.ability.name).join(" | "),
-pokemon.stats.map(a => a.base_stat).join(" | "))  */
-/* pokemon.types.forEach(a => console.log(pokemon.name, a.type.name)) */
-/* 
-pokemons => {
-    console.log(pokemons[0], `https://pokeapi.co/api/v2/pokemon-species/0/`)
-    let cardsPokemon = pokemons.reduce((accumulator, {name, id, types, abilities, stats, height, weight}) => {
-        let tipos = types.map(typeInfo => typeInfo.type.name).join(" | ");
-        let tiposClass = types.map(typeInfo => typeInfo.type.name);
-        let imagem = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
-        let habilidades = abilities.map(abilInfo => abilInfo.ability.name).join(" | ");
-        let estatisticasSep = stats.map(a => a.base_stat);
-        accumulator += 
-        `
-            <div class="card ${tiposClass[0]}">
-                <div class="fundo">
-                    <img src=${imagem}>
-                </div>
-                <div>
-                    <p>${id}. ${name[0].toUpperCase() + name.substring(1)}</p>
-                    <p>${tipos}</p>
-                    <p> 
-                        height: ${height/10} m<br>
-                        weight: ${weight/10} kg
-                    </p>
-                </div>
-                <div>
-                    <p>Habilidades:</p>
-                    <p>${habilidades}</p>
-                </div>
-                <div>
-                <p>Estatísticas:</p>
-                    <ul>
-                        <li>HP.:${estatisticasSep[0]}</li>
-                        <li>ATK:${estatisticasSep[1]}</li>
-                        <li>DEF:${estatisticasSep[2]}</li>
-                        <li>SP. ATK:${estatisticasSep[3]}</li>
-                        <li>SP. DEF:${estatisticasSep[4]}</li>
-                        <li>SPEED:${estatisticasSep[5]}</li>
-                    </ul>
-                </div>
-                
-            </div>
-        `
-        return accumulator
-    }, '')
-    conjuntoCards.innerHTML = cardsPokemon
- */
